a3d5bcf6a5d04037316c4091fe78766ed8bfd1e3
Priyabrata Das <developer@farmvizion.com>
2025-05-19 01:28:11 +0200

    rev 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/myfarm/', // This is important for GitHub Pages routing
+  plugins: [react()],
+  base: '/myfarm/',
 })
