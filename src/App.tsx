f626451fba7cd8feae95bf72fcd60fce9948a8e4
Priyabrata Das <developer@farmvizion.com>
2025-05-19 00:30:39 +0200

    rev 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
-import Navbar from './components/Navbar'
 import Team from './pages/Team'
-
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
   )
 }
 
