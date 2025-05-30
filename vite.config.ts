fffa476d2f99b8c40d84d680793102aaf93cd582
Priyabrata Das <developer@farmvizion.com>
2025-05-30 06:07:52 +0200

    v 42

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/myfarm/',
+  build: {
+    manifest: true, // generates manifest.json for cache busting if needed
+  }
 })
